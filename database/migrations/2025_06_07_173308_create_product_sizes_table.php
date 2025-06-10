7e60d9cd7fc2e20370708ac7300b29da3dd856d4
= <=>
2025-06-10 10:52:57 +0000

    Initila Database Creation and Models

--- database/migrations/2025_06_07_173308_create_product_sizes_table.php
+++ database/migrations/2025_06_07_173308_create_product_sizes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('product_sizes', function (Blueprint $table) {
             $table->id();
+            $table->string("name")->nullable();
+            $table->string("slug")->nullable();
+            $table->string("image")->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2025_06_07_183405_create_products_table.php
+++ database/migrations/2025_06_07_183405_create_products_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\ProductType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,22 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Category::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(ProductType::class)->constrained()->nullOnDelete()->nullable();
+            $table->string('name')->nullable();
+            $table->enum("gender", ["Male", "Female"]);
+            $table->boolean("new")->default(false);
+            $table->boolean("active")->default(true);
+            $table->boolean("sale")->default(false);
+            $table->integer("rate")->default(0);
+            $table->integer("price")->default(0);
+            $table->integer("originPrice")->default(0);
+            $table->foreignIdFor(Brand::class)->nullable()->constrained()->nullOnDelete();
+            $table->string('slug')->unique()->nullable();
+            $table->integer("sold")->default(0);
+            $table->integer("qunatity")->default(0);
+            $table->text('description')->nullable();
+            $table->string("action")->default("buy now");
             $table->timestamps();
         });
     }
