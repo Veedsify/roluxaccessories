4a98a88f3a6380e331c1df2c70ef69666522ac0e
= <=>
2025-06-22 18:33:14 +0100

    feat: Update font family to Instrument Sans and optimize font loading in head section

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,9 +6,9 @@
 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<link href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,900&display=swap" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('frontend/css/swiper-bundle.min.css')}}" />
 <link rel="stylesheet" href="{{ asset('frontend/css/style.css')}}" />
 <link rel="stylesheet" href="{{ asset('dist/output-scss.css')}}" />
